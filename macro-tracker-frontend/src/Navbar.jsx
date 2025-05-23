d04bb4da7a0223494d74cab4f5ceb72c75aa882c
Hernerdez <rhjrgoalusa@gmail.com>
2025-05-23 01:58:42 -0500

    need to be logged in to use food search

--- macro-tracker-frontend/src/Navbar.jsx
+++ macro-tracker-frontend/src/Navbar.jsx
@@ -11,19 +11,20 @@ const Navbar = () => {
 
   return (
     <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
-      <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
-      <Link to="/search" style={{ marginRight: '1rem' }}>Food Search</Link> {/* ✅ NEW */}
+  <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
 
-      {!token && <Link to="/signup" style={{ marginRight: '1rem' }}>Signup</Link>}
-      {!token && <Link to="/login" style={{ marginRight: '1rem' }}>Login</Link>}
-      {token && <Link to="/dashboard" style={{ marginRight: '1rem' }}>Dashboard</Link>}
+  {!token && <Link to="/signup" style={{ marginRight: '1rem' }}>Signup</Link>}
+  {!token && <Link to="/login" style={{ marginRight: '1rem' }}>Login</Link>}
+  {token && <Link to="/dashboard" style={{ marginRight: '1rem' }}>Dashboard</Link>}
+  {token && <Link to="/search" style={{ marginRight: '1rem' }}>Food Search</Link>}
 
-      {token && (
-        <button onClick={handleLogout} style={{ marginLeft: '1rem' }}>
-          Logout
-        </button>
-      )}
-    </nav>
+  {token && (
+    <button onClick={handleLogout} style={{ marginLeft: '1rem' }}>
+      Logout
+    </button>
+  )}
+</nav>
+
   );
 };
 
