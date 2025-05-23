4c96ee58bfadbcb5caaf54707914b356e76c19fa
Hernerdez <rhjrgoalusa@gmail.com>
2025-05-22 20:34:03 -0500

    Dashboard after loggin and and protect route

--- macro-tracker-frontend/src/App.jsx
+++ macro-tracker-frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
 import Signup from './Signup';
-import Login from './Login'; // You can add this if you haven’t already
+import Login from './Login';
+import Dashboard from './Dashboard';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
   return (
     <Routes>
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
