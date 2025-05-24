9608e255ddb8c1e34c11e69a61a19375c53c88f7
Hernerdez <rhjrgoalusa@gmail.com>
2025-05-23 22:12:45 -0500

    date import in models

--- macro_tracker_backend/models.py
+++ macro_tracker_backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float, Date
 from sqlalchemy.orm import relationship
 from .database import Base
 
