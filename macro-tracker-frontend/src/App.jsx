94f7b4fefb671a4425151ecef973cdd9be71f45c
Hernerdez <rhjrgoalusa@gmail.com>
2025-05-27 00:24:00 -0500

    landing page

--- macro-tracker-frontend/src/App.jsx
+++ macro-tracker-frontend/src/App.jsx
@@ -6,14 +6,14 @@ import Dashboard from './Dashboard';
 import ProtectedRoute from './ProtectedRoute';
 import Navbar from './Navbar';
 import FoodSearch from './FoodSearch';
-import LandingPage from './LandingPage'; // make sure it’s correctly imported!
+import LandingPage from './LandingPage'; // make sure it's correctly imported!
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<LandingPage />} /> {/* swapped! */}
+        <Route path="/" element={<LandingPage />} /> {/* This should be your home route */}
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route
